c3babb42b844b100d3ece986934705788d5889e6
BiaTrivillin <biatrivillin.code@gmail.com>
2024-03-27 19:31:32 -0300

    created header style

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,10 +1,34 @@
 import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FiMenu } from "react-icons/fi";
+import { useState } from "react";
 
 function Header() {
+
+    const [navStyle, setNavStyle] = useState("");
+
+    const showNav = () => {
+        if (navStyle != styles.show_navigation) setNavStyle(styles.show_navigation);
+        else setNavStyle(styles.hide_navigation);
+    };
+
     return(
-        <header>
+        <header className={styles.header}>
             <h1><Link to="/">SPIN YOUR<br/> SOUND</Link></h1>
-            <nav>
+            <form>
+                <input className={styles.search_input} type="text" placeholder="what do you want to hear today?"/>
+                <button className={styles.search_btn}><FaMagnifyingGlass /></button>
+            </form>
+            <button onClick={showNav} className={styles.nav_btn}><FiMenu /></button>
+            <nav className={`${styles.navigation} ${navStyle}`}>
+                <ul>
+                    <li><Link to="/">home</Link></li>
+                    <li><Link to="/catalog">catalog</Link></li>
+                    <li><a href="https://www.linkedin.com/in/beatriz-trivillin/" rel="noreferrer" target="_blank">contact</a></li>
+                </ul>
+            </nav>
+            <nav className={styles.navigation_desktop}>
                 <ul>
                     <li><Link to="/">home</Link></li>
                     <li><Link to="/catalog">catalog</Link></li>
