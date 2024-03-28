bdd92e4c674df90f059c5ba4ec82abbd6b209837
BiaTrivillin <biatrivillin.code@gmail.com>
2024-03-28 13:39:36 -0300

    created banners with css style

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,10 +1,12 @@
 import Presentation from "./presentation/Presentation";
+import Banner from "./banner/Banner";
 
 function Home() {
 
     return (
         <>
             <Presentation/>
+            <Banner/>
         </>
     );
 }
---
+++ src/components/pages/home/banner/Banner.tsx
@@ -0,0 +1,34 @@
+import bannerOneImg from "../../../../assets/banner-one.png";
+import bannerTwoImg from "../../../../assets/banner-two.png";
+import bannerThreeImg from "../../../../assets/banner-three.png";
+import styles from "./Banner.module.css";
+
+function Banner() {
+    return(
+        <>
+            <div className={`${styles.banner} ${styles.banner_one}`}>
+                <img src={bannerOneImg}/>
+                <div>
+                    <h1>VINYL</h1>
+                    <p>The must-have accessory for your musical wardrobe.</p>
+                </div>
+            </div>
+            <div className={`${styles.banner} ${styles.banner_two}`}>
+                <img src={bannerTwoImg}/>
+                <div>
+                    <h1>Discover your beat for much less.</h1>
+                    <p>Incredible daily deals await!</p>
+                </div>
+            </div>
+            <div className={`${styles.banner} ${styles.banner_three}`}>
+                <img src={bannerThreeImg}/>
+                <div>
+                    <h1>Your musical taste is top-notch!</h1>
+                    <p>Come savor our latest offerings.</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Banner;
