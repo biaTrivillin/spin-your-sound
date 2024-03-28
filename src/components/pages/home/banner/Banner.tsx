353e58593b77d7ceaba196f721ee244e6168d58a
BiaTrivillin <biatrivillin.code@gmail.com>
2024-03-28 15:31:47 -0300

    created carrousel animation

--- src/components/pages/home/banner/Banner.tsx
+++ src/components/pages/home/banner/Banner.tsx
@@ -2,32 +2,41 @@ import bannerOneImg from "../../../../assets/banner-one.png";
 import bannerTwoImg from "../../../../assets/banner-two.png";
 import bannerThreeImg from "../../../../assets/banner-three.png";
 import styles from "./Banner.module.css";
+// import "./bbbb.css";
 
 function Banner() {
     return(
-        <>
-            <div className={`${styles.banner} ${styles.banner_one}`}>
-                <img src={bannerOneImg}/>
-                <div>
-                    <h1>VINYL</h1>
-                    <p>The must-have accessory for your musical wardrobe.</p>
-                </div>
+        <section className={styles.carousel}>
+            <div className={styles.banners_container}>
+                <article>
+                    <div className={`${styles.banner} ${styles.banner_one}`}>
+                        <img src={bannerOneImg}/>
+                        <div>
+                            <h1>VINYL</h1>
+                            <p>The must-have accessory for your musical wardrobe.</p>
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <div className={`${styles.banner} ${styles.banner_two}`}>
+                        <img src={bannerTwoImg}/>
+                        <div>
+                            <h1>Discover your beat for much less.</h1>
+                            <p>Incredible daily deals await!</p>
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <div className={`${styles.banner} ${styles.banner_three}`}>
+                        <img src={bannerThreeImg}/>
+                        <div>
+                            <h1>Your musical taste is top-notch!</h1>
+                            <p>Come savor our latest offerings.</p>
+                        </div>
+                    </div>
+                </article>
             </div>
-            <div className={`${styles.banner} ${styles.banner_two}`}>
-                <img src={bannerTwoImg}/>
-                <div>
-                    <h1>Discover your beat for much less.</h1>
-                    <p>Incredible daily deals await!</p>
-                </div>
-            </div>
-            <div className={`${styles.banner} ${styles.banner_three}`}>
-                <img src={bannerThreeImg}/>
-                <div>
-                    <h1>Your musical taste is top-notch!</h1>
-                    <p>Come savor our latest offerings.</p>
-                </div>
-            </div>
-        </>
+        </section>
     );
 }
 
