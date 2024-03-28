427f723755bdfded2b498775a1ad92df67e76b7e
BiaTrivillin <biatrivillin.code@gmail.com>
2024-03-28 11:12:16 -0300

    changed font-size in index css file

--- src/components/pages/home/presentation/Presentation.tsx
+++ src/components/pages/home/presentation/Presentation.tsx
@@ -1,13 +1,11 @@
 import presentationRecord from "../../../../assets/presentation-record.png";
 import styles from "./Presentation.module.css";
-import needle from "../../../../assets/needle.png";
 
 function Presentation() {
 
     return (
         <div className={styles.presentation_container}>
             <img className={styles.presentation_img} src={presentationRecord}/>
-            <img className={styles.needle_img} src={needle}/>
             <div className={styles.presentation_text}>
                 <h1>SPIN YOUR <br/>SOUND</h1>
                 <h2>Find Your Beat, Let Music Speak!</h2>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import "./reset.css";
+import "./styles/index.css";
+import "./styles/reset.css";
 import MainRoutes from "./routes";
 
 
