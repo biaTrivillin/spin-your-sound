2a4fbf4b75dd11972b2c32378b8faaa873d21051
BiaTrivillin <biatrivillin.code@gmail.com>
2024-04-02 11:01:30 -0300

    created home.css file

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -4,18 +4,19 @@ import FilterCards from "./filterCards/FilterCards";
 import CatalogBanner from "./catalogBanner/CatalogBanner";
 import BenefitsBanner from "./benefitsBanner/BenefitsBanner";
 import Recommendations from "./recommendations/Recommendations";
+import styles from "./Home.module.css";
 
 function Home() {
 
     return (
-        <>
+        <main className={styles.content_container}>
             <Presentation/>
             <BannerCarousel/>
             <FilterCards/>
             <CatalogBanner/>
             <Recommendations/>
             <BenefitsBanner/>
-        </>
+        </main>
     );
 }
 
